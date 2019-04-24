0fa49b9718128b0af163fe3c14a4461239243e4e
AugustineOchieng <gusochieng@gmail.com>
2019-04-24 12:30:20 +0300

    Test comments model

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,5 +10,16 @@ class UserModelTest(unittest.TestCase):
       self.new_user.password
   def test_password_verification(self):
     self.assertTrue(self.new_user.verify_password('gus9999#'))
+class CommentsModelTest(unittest.TestCase):
 
+    def setUp(self):
+       self.new_comment = Comments(id=1, user_id = 2, comment = 'person of interest',pitch_id = '5',date_posted='2019-04-24')
+
+    def test_comment_variables(self):
+       self.assertEquals(self.new_comment.comment,'person of interest')
+       self.assertEquals(self.new_comment.date_posted,'2019-04-24')
+       self.assertEquals(self.new_comment.user_id, 2)
+    def test_save_comment(self):
+
+        self.assertTrue(len(Comments.query.all())>0)
 
