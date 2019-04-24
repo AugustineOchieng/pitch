9c8d22f384819d48cc655e5073325b9e16a83b46
AugustineOchieng <gusochieng@gmail.com>
2019-04-24 12:50:01 +0300

    Remove test_save tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,29 +1,34 @@
 import unittest
 from app.models import User, Comments, Pitches
-class UserModelTest(unittest.TestCase):
-  def setUp(self):
-    self.new_user = User(password='gus9999#')
-  def test_password_setter(self):
-    self.assertTrue(self.new_user.password_hash is not None)
-  def test_no_access_password(self):
-    with self.assertRaises(AttributeError):
-      self.new_user.password
-  def test_password_verification(self):
-    self.assertTrue(self.new_user.verify_password('gus9999#'))
-class CommentsModelTest(unittest.TestCase):
 
+
+class UserModelTest(unittest.TestCase):
     def setUp(self):
-       self.new_comment = Comments(id=1, user_id = 2, comment = 'person of interest',pitch_id = '5',date_posted='2019-04-24')
+        self.new_user = User(password="gus9999#")
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password_hash is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password("gus9999#"))
+
+
+class CommentsModelTest(unittest.TestCase):
+    def setUp(self):
+        self.new_comment = Comments(
+            id=1,
+            user_id=2,
+            comment="person of interest",
+            pitches_id="5",
+            date_posted="2019-04-24",
+        )
 
     def test_comment_variables(self):
-       self.assertEquals(self.new_comment.comment,'person of interest')
-       self.assertEquals(self.new_comment.date_posted,'2019-04-24')
-       self.assertEquals(self.new_comment.user_id, 2)
-    def test_save_comment(self):
-
-        self.assertTrue(len(Comments.query.all()) > 0)
-class PitchesModelTest(unittest.TestCase):
-
-    def test_save_pitch(self):
-        self.assertTrue(len(Pitches.query.all())>0)
+        self.assertEquals(self.new_comment.comment, "person of interest")
+        self.assertEquals(self.new_comment.date_posted, "2019-04-24")
+        self.assertEquals(self.new_comment.user_id, 2)
 
