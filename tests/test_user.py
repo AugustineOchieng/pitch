ee8eabc293ef7160cbda34beded83b56907707a9
AugustineOchieng <gusochieng@gmail.com>
2019-04-24 12:26:00 +0300

    Modify password

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,6 +9,6 @@ class UserModelTest(unittest.TestCase):
     with self.assertRaises(AttributeError):
       self.new_user.password
   def test_password_verification(self):
-    self.assertTrue(self.new_user.verify_password('banana'))
+    self.assertTrue(self.new_user.verify_password('gus9999#'))
 
 
