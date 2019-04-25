b02c6c800fc51370fefc5873948e364f9e63aa7d
AugustineOchieng <gusochieng@gmail.com>
2019-04-25 11:06:13 +0300

    Change configurations to production

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,19 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required
 
 
 class PitchForm(FlaskForm):
-    title = StringField('Pitch Title')
-    category = SelectField(u'Pitch Category', choices=[('life', 'life'), ('coding', 'coding'), ('funny', 'funny')])
-    pitch = TextAreaField('Pitch')
-    submit = SubmitField('Submit')
+    title = StringField("Pitch Title")
+    category = SelectField(
+        u"Pitch Category",
+        choices=[
+            ("interviews", "interviews"),
+            ("auditions", "auditions"),
+            ("impressions", "impressions"),
+        ],
+    )
+    pitch = TextAreaField("Pitch")
+    submit = SubmitField("Submit")
+
 
 class CommentForm(FlaskForm):
 
-    comment = TextAreaField('Comment')
-    submit = SubmitField('Post Comments')
+    comment = TextAreaField("Comment")
+    submit = SubmitField("Post Comments")
+
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('More info.',validators = [Required()])
-    submit = SubmitField('Submit')
+    bio = TextAreaField("More info.", validators=[Required()])
+    submit = SubmitField("Submit")
+
